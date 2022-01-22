426787ca2171d05a8df7fad3d086dd010031b1ff
tsrinu28852 <tsrinu28852@gmail.com>
2022-01-22 11:57:21 +0100

    Update HelloWorld.java
    add tadela

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello Srinivas Tadela!");
-    System.out.println("Hello Sunamya!");
-    System.out.println("Hello Yogith!");
+    System.out.println("Hello Sunamya Tadela!");
+    System.out.println("Hello Yogith Tadela!");
     System.out.println("Hello Deepika Tadela!");
     System.out.println("Hello Prabha Tadela!");
     System.out.println("Hello Lakshmi Narayana Tadela!");
